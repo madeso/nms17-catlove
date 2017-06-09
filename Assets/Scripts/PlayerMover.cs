6ea6dbf28ab931bc1f2c3f860158ca5219d7ae80
Gustav <sir.gustav.the.coder@gmail.com>
2017-06-09 17:12:30 +0200

    player movement

--- Assets/Scripts/PlayerMover.cs
+++ Assets/Scripts/PlayerMover.cs
@@ -4,13 +4,28 @@ using UnityEngine;
 
 public class PlayerMover : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+	public float speed = 10.0f;
+
+	private Rigidbody rb;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody>();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	void FixedUpdate ()
+	{
+		var theCamera = Camera.main;
+		Vector3 movement = Input.GetAxis("Vertical") * theCamera.transform.forward + Input.GetAxis("Horizontal") * theCamera.transform.right;
+
+		if(movement.sqrMagnitude > 0.1f ) {
+			movement.Normalize();
+			rb.AddForce (movement * speed);
+		}
+
+		var v = rb.velocity;
+		if(v.sqrMagnitude > speed * speed ) {
+			rb.velocity = v.normalized * speed;
+		}
 	}
 }
