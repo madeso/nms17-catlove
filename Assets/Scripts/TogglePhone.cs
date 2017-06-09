a5ef3f43c894808ff482f6504157472e4bbe17f7
Gustav <sir.gustav.the.coder@gmail.com>
2017-06-09 23:17:12 +0200

    aded basic dogs app

---
+++ Assets/Scripts/TogglePhone.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TogglePhone : MonoBehaviour {
+	GameObject dogr;
+	bool status = false;
+
+	// Use this for initialization
+	void Start () {
+		this.dogr = GameObject.Find("DogR");
+		this.dogr.SetActive(status);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButtonDown("Fire1")) {
+			status = !status;
+			this.dogr.SetActive(status);
+		}
+	}
+}
