6e873d0ab36195fda699a20ec5d33020234ac6c8
Gustav <sir.gustav.the.coder@gmail.com>
2017-06-09 21:23:59 +0200

    movement

--- Assets/Scripts/PlayerMover.cs
+++ Assets/Scripts/PlayerMover.cs
@@ -8,9 +8,13 @@ public class PlayerMover : MonoBehaviour {
 
 	private Rigidbody rb;
 
+	public Transform RotationRoot = null;
+	public Wiggle wiggle;
+
 	void Start ()
 	{
 		rb = GetComponent<Rigidbody>();
+		wiggle = GetComponentInChildren<Wiggle>();
 	}
 
 	void FixedUpdate ()
@@ -18,9 +22,24 @@ public class PlayerMover : MonoBehaviour {
 		var theCamera = Camera.main;
 		Vector3 movement = Input.GetAxis("Vertical") * theCamera.transform.forward + Input.GetAxis("Horizontal") * theCamera.transform.right;
 
+		movement.y = 0.0f;
+
+		bool move = false;
+
 		if(movement.sqrMagnitude > 0.1f ) {
+			move = true;
 			movement.Normalize();
 			rb.AddForce (movement * speed);
+
+			Quaternion newRotation = Quaternion.LookRotation(movement);
+			// this.rb.MoveRotation(newRotation);
+			if(this.RotationRoot != null) {
+				this.RotationRoot.rotation = newRotation;
+			}
+		}
+
+		if(wiggle != null) {
+			wiggle.IsWiggle = move;
 		}
 
 		var v = rb.velocity;
---
+++ Assets/Scripts/Wiggle.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wiggle : MonoBehaviour {
+	public bool IsWiggle = false;
+	public float WiggleTime = 0.1f;
+	public float WiggleRange = 20.0f;
+
+	public int state = 0;
+	public float time = 0.0f;
+	private bool lastwiggle = false;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(IsWiggle != lastwiggle) {
+			if(IsWiggle) {
+				state = Random.Range(0,2);
+				if(state == 0) state = -1;
+			}
+			else {
+				state = 0;
+			}
+			lastwiggle = IsWiggle;
+		}
+
+		if(state != 0 ) {
+			time -= Time.deltaTime;
+		}
+		switch(state) {
+		case 0:
+			break;
+		case -1:
+		case 1:
+			if(time < 0 ) {
+				state = -state;
+				time += WiggleTime;
+			}
+			break;
+		}
+
+		var w = state * WiggleRange;
+		transform.localRotation = Quaternion.Euler(new Vector3(0, 0, w));
+	}
+}
