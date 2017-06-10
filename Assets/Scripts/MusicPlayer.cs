dfff62d1dd7401333b0711ef03832ea778657efd
Gustav <sir.gustav.the.coder@gmail.com>
2017-06-11 01:25:32 +0200

    Added music

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour {
+	public AudioClip MusicLoop;
+	public AudioSource Source;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Source.isPlaying == false ) {
+			Source.clip = MusicLoop;
+			Source.loop = true;
+			Source.Play();
+		}
+	}
+}
