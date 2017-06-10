dfb7058e06dc2310058e6a08bc004e08843c890a
Gustav <sir.gustav.the.coder@gmail.com>
2017-06-10 22:40:47 +0200

    Different materials on pedestrians

---
+++ Assets/Scripts/SetupRandomFur.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetupRandomFur : MonoBehaviour {
+	MeshRenderer rend;
+
+	public Material[] CatMaterials;
+
+	static void ReplaceMaterial(MonoBehaviour go, string matname, Material newmat) {
+		var r = go.GetComponentsInChildren<MeshRenderer>();
+		foreach(var rend in r) {
+			if (true) {
+				// var m = rend.materials[i];
+				var m = rend.material;
+				var n = m.name.Trim().ToLower();
+				var mn = matname.Trim().ToLower();
+				// print(n);
+				// print(mn);
+				if( n.StartsWith(mn) ) {
+					print("changing mat");
+					// rend.materials[i] = newmat;
+					rend.material = newmat;
+				}
+			}
+		}
+	}
+
+	void Start () {
+		// print("hello");
+		var newmat = this.CatMaterials[ Random.Range(0, this.CatMaterials.Length) ];
+		ReplaceMaterial(this, "CatMat", newmat);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
