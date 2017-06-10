52ad20ce3c51cbe928a81028db1606adb6c66c40
Gustav <sir.gustav.the.coder@gmail.com>
2017-06-10 16:06:54 +0200

    Added message screen

--- Assets/Scripts/DogrProfile.cs
+++ Assets/Scripts/DogrProfile.cs
@@ -4,24 +4,50 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class DogrProfile : MonoBehaviour {
-	public Text Title;
-	public Text Description;
-	public Image MainImage;
+	public Text[] Title;
+	public Text[] Description;
+	public Image[] MainImage;
+
+	public Text MatchMessage;
+	public Text UnmatchReason;
 
 	public Sprite[] Images;
 	Tracery.Grammar grammar;
 
 	public void GenerateNewProfile() {
-		this.MainImage.sprite = this.Images[Random.Range(0, this.Images.Length)];
-		this.Title.text = this.grammar.Flatten("#title#");
-		this.Description.text = this.grammar.Flatten("#desc#");
+		var sprite = this.Images[Random.Range(0, this.Images.Length)];
+		var title = this.grammar.Flatten("#title#");
+		var desc = this.grammar.Flatten("#desc#");
+
+		foreach(var s in this.MainImage) s.sprite = sprite;
+		foreach(var t in this.Title) t.text = title;
+		foreach(var d in this.Description) d.text = desc;
+
+		GenerateMatchedData();
+	}
+
+	void GenerateMatchedData() {
+		var okMessage = this.grammar.Flatten("#okmessage#");
+		var okreason = "You were unmatched.";
+
+		var badMessage = this.grammar.Flatten("#badmessage#");
+		var badreason = "You blocked user.";
+
+		if(Random.value > 0.5f ) {
+			MatchMessage.text = okMessage;
+			UnmatchReason.text = okreason;
+		}
+		else {
+			MatchMessage.text = badMessage;
+			UnmatchReason.text = badreason;
+		}
 	}
 
 	// Use this for initialization
 	void Start () {
-		TextAsset jsonFile = (TextAsset) Resources.Load("profile"); // assuming the file is at Assets/Resources/grammar.json
+		TextAsset jsonFile = (TextAsset) Resources.Load("profile");
 		this.grammar = Tracery.Grammar.LoadFromJSON(jsonFile);
-		// this.grammar = this.GetComponent<TraceryGrammar>().Grammar;
+		GenerateNewProfile();
 	}
 	
 	// Update is called once per frame
---
+++ Assets/Scripts/PhoneScreens.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhoneScreens : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
